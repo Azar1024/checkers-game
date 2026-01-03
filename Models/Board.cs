584f40b2bb6edc0004e359ba2b84e96f7ab09d1a
Hewow984 <hewow984@gmail.com>
2026-01-03 20:52:25 +0700

    Добавлено: метод GetLegalMoves для проверки возможных ходов

--- Models/Board.cs
+++ Models/Board.cs
@@ -32,10 +32,48 @@ public class Board
         this[move.FromRow, move.FromCol] = null;
         this[move.ToRow, move.ToCol] = piece;
 
+        // Удаление захваченной шашки
+        if (move.IsCapture)
+        {
+            int dr = (move.ToRow - move.FromRow) / 2;
+            int dc = (move.ToCol - move.FromCol) / 2;
+            this[move.FromRow + dr, move.FromCol + dc] = null;
+        }
+
         // Превращение в дамку
         if (piece?.Owner == Player.White && move.ToRow == 0)
             this[move.ToRow, move.ToCol] = piece with { Type = PieceType.King };
         if (piece?.Owner == Player.Black && move.ToRow == Size - 1)
             this[move.ToRow, move.ToCol] = piece with { Type = PieceType.King };
     }
+
+    public IEnumerable<Move> GetLegalMoves(int row, int col, Player current)
+    {
+        var piece = this[row, col];
+        if (piece?.Owner != current) yield break;
+
+        var dirs = piece.IsKing
+            ? new[] { (-1, -1), (-1, 1), (1, -1), (1, 1) }
+            : piece.IsWhite
+                ? new[] { (-1, -1), (-1, 1) }
+                : new[] { (1, -1), (1, 1) };
+
+        foreach (var (dr, dc) in dirs)
+        {
+            // Обычный ход
+            int nr = row + dr, nc = col + dc;
+            if (IsInside(nr, nc) && this[nr, nc] == null)
+                yield return new Move(row, col, nr, nc);
+
+            // Прыжок
+            int jr = row + 2 * dr, jc = col + 2 * dc;
+            if (IsInside(nr, nc) && IsInside(jr, jc) &&
+                this[nr, nc]?.Owner == Opponent(current) &&
+                this[jr, jc] == null)
+                yield return new Move(row, col, jr, jc);
+        }
+    }
+
+    private static Player Opponent(Player p) => p == Player.White ? Player.Black : Player.White;
+    private static bool IsInside(int r, int c) => r >= 0 && r < Size && c >= 0 && c < Size;
 }
