c0d41e42ba9fe6c67aff10e8f599213c2afa3296
BenzinX <kleontyev@list.ru>
2026-01-07 16:20:32 +0700

    Очистил артефакты сборки

--- CheckersClient/obj/Release/net8.0/CheckersGame.AssemblyInfo.cs
+++ CheckersClient/obj/Release/net8.0/CheckersGame.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("CheckersGame")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36e85df6543ce6a6f3a7149f01bcfcdc3d70ee0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CheckersGame")]
